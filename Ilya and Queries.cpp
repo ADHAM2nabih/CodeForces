7ee67ab3bb0d1300c14ffcdb7827b7f61ae77d56
Adham mohamed <117783162+ADHAM2nabih@users.noreply.github.com>
2024-06-03 03:34:43 +0300

    Update Ilya and Queries.cpp

--- Ilya and Queries.cpp
+++ Ilya and Queries.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+    
     string s;
-    cin>>s;
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int l,r,cnt=0;
-        cin>>l>>r;
-        for(int i = l-1;i<r-1;i++)
-        {
-            if(s[i]==s[i+1])cnt++;
-        }
-        cout<<cnt<<endl;
+    cin >> s;
+    int n = s.length();
 
+    vector<int> pre(n, 0);
+    for (int i = 1; i < n; ++i) {
+        pre[i] = pre[i - 1] + (s[i] == s[i - 1] ? 1 : 0);
     }
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int l, r;
+        cin >> l >> r;
+        
+        cout << pre[r - 1] - pre[l - 1] << endl;
+    }
+    
+    return 0;
 }
